84de900c5c083c71c35b53972af9582826e9883d
TiloWIjkniet <113525798+TiloWIjkniet@users.noreply.github.com>
2022-09-15 11:14:12 +0200

    addit test script

--- test Gethub/Assets/Scripts/Test.cs
+++ test Gethub/Assets/Scripts/Test.cs
@@ -6,6 +6,8 @@ public class Test : MonoBehaviour
 {
 
     public Transform transform;
+    public float speed = 1f;
+    public Vector3 derection  = new Vector3(0,1,0);
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,6 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0,1,0) ;
+        transform.position += derection * speed * Time.deltaTime ;
     }
 }
