8d554567d3ad9230ae73701bddbb047f2d1493f5
TiloWIjkniet <113525798+TiloWIjkniet@users.noreply.github.com>
2022-09-22 17:31:48 +0200

    update

--- test Gethub/Assets/Scripts/Car/EnterCar.cs
+++ test Gethub/Assets/Scripts/Car/EnterCar.cs
@@ -4,55 +4,5 @@ using UnityEngine;
 
 public class EnterCar : MonoBehaviour
 {
-   public GameObject Player;
-    public GameObject CarCamera;
-    public Animator animator;
-    public DriveCar driveCar;
-    public Transform exsitPosition;
-
-    bool inCar = false;
-
-    void Start()
-    {
-        //Player = GameObject.FindWithTag("Player");
-    }
-
-    void OnTriggerStay(Collider other)
-    {     
-        if (other.name =="PlayerOBJ" && Input.GetKeyDown(KeyCode.E) && inCar == false)
-        {
-           StartCoroutine(enterCar());
-        }
-    }
-
-    void Update(){
-        if (inCar == true && driveCar.SpeedCalculator() <= 15 && Input.GetKeyDown(KeyCode.E))
-        {
-            StartCoroutine(exsitCar());
-        }
-    }
-
-    IEnumerator  exsitCar()
-    {
-        
-        animator.SetTrigger("Exsit");
-        yield return new WaitForSeconds(1f);
-        CarCamera.SetActive(false);
-        Player.SetActive(true);
-        driveCar.enabled = false;
-        Player.transform.position = exsitPosition.position; 
-        Player.transform.rotation = Quaternion.LookRotation (transform.position - Player.transform.position);
-        inCar = false;
-        
-    }
-
-      IEnumerator enterCar ()
-      {
-        animator.SetTrigger("Enter");
-        Player.SetActive(false);
-        CarCamera.SetActive(true);
-        yield return new WaitForSeconds(1f);
-        inCar = true;        
-        driveCar.enabled = true;
-      }
+   
 }
