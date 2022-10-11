23d0e04e139b42545ba7d21d2e83bc9f0b00fd85
TiloWIjkniet <113525798+TiloWIjkniet@users.noreply.github.com>
2022-10-11 09:17:44 +0200

    ha

--- test Gethub/Assets/Scripts/Teleporter/Teleporter.cs
+++ test Gethub/Assets/Scripts/Teleporter/Teleporter.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class Teleporter : MonoBehaviour
 {
 
-    public Vector3 destination ;
+    public Vector3 vecDestination ;
     public ParticleSystem ParticleSystem;
+    public Transform transDestination;
+    public Vector3 destination;
     void OnTriggerEnter(Collider other)
     {   
         
 
+        
+        if(!transform == null);
+        {
+            destination = transDestination.position;
+                
+        }
+        else  
+        {
+            destination = vecDestination;
+        }
         StartCoroutine(Wait(other));
         
     }   
-     IEnumerator Wait(Collider other)
+    IEnumerator Wait(Collider other)
     {   other.transform.parent.position = destination;
         
         var par = Instantiate(ParticleSystem, other.transform.parent.position+  other.transform.parent.forward, transform.rotation );
